91e97e361400fd067ea12707ae592ea159f73b58
regaleriatati <tatianalaurich@hotmail.com>
2025-09-22 18:41:33 -0300

    commit

--- regaleria-tati/src/App.jsx
+++ regaleria-tati/src/App.jsx
@@ -1,5 +1,5 @@
-import NavBar from "./components/NavBar";
-import ItemListContainer from "./components/ItemListContainer";
+import NavBar from "./components/navbar";
+import ItemListContainer from "./components/itemlistcontainer";
 
 function App() {
   return (
--- regaleria-tati/src/components/navbar.jsx
+++ regaleria-tati/src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import CartWidget from "./CartWidget";
+import CartWidget from "./cartwidget";
 
 function NavBar() {
     return (
